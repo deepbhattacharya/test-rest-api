1c85f019041ca1f7c5374cacbfebe5462de5dce1
Deep Bhattacharya <deep_bhattacharya@yahoo.com>
2021-02-20 02:42:00 +0530

    Added config checks for env var and properties reader

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
+  PropertiesReader = require('properties-reader'),
   RawData = require('./api/models/testModel'), //created model loading here
   bodyParser = require('body-parser');
 
-
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/local'); 
+// if remote setup with env vars setup (e.g. in heroku)
+if (process.env.MONGO_CONN_STRING) {
+  mongoose.connect(process.env.MONGO_CONN_STRING);
+} else {
+  // local installation
+  const prop = PropertiesReader('./project.properties');
+  mongoose.connect(prop.get("main.MONGO_CONN_STRING"));
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.raw({ type: '*/*' }));
@@ -18,4 +25,4 @@ routes(app); //register the route
 
 app.listen(port);
 
-console.log(`todo list RESTful API server started on: ${port}`);
+console.log(`Raw Data Ingestor RESTful API server started on: ${port}`);
